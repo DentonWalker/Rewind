369687920893bd949a1bb2c47fe5d5d6bd2fe9be
Denton Walker <lane.walker1@gmail.com>
2024-05-05 21:27:30 -0400

    updated code page

--- js/site.js
+++ js/site.js
@@ -2,32 +2,20 @@
 //Controller Function
 function getValue() {
   document.getElementById("alert").classList.add("invisible");
-
   let userString = document.getElementById("userString").value;
-
   let revString = reverseString(userString);
-
   displayString(revString);
 }
-
 //Reverse the string
 //Logic Function
 function reverseString(userString) {
   let revString = [];
-
-  //let name = Lane
-  //name[0] = "L";
-  //name[3] = "e";
-  //last position in an array is name.length - 1
-
   // reverse a string using a for loop
   for (let index = userString.length - 1; index >= 0; index--) {
     revString += userString[index];
   }
-
   return revString;
 }
-
 //Display the reversed string to the user
 //View Function
 function displayString(revString) {
